43dbe519452a8b92bd4fae9031ac183dfc46a325
Ajay Reddy <126524783+Azzuboy@users.noreply.github.com>
2026-04-01 20:38:03 -0500

    Add random item endpoint

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var natureReservesRouter = require('./routes/naturereserves');
 var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/naturereserves', natureReservesRouter);
 app.use('/grid', gridRouter);
+app.use('/selector', pickRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
